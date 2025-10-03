b7d5752ca21c75dc1fddb91b56dbd21f415f8fdd
Trường Vĩnh Trịnh <truong.vinh.trinh@mgm-tp.com>
2025-10-03 20:37:02 +0700

    Update header.tsx

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -8,7 +8,7 @@ export const HeaderSection = () => {
 			<nav className="main-nav">
 				<ul className="main-nav-list">
 					<li className="main-nav-item">
-						<a href="https://www.facebook.com/truong.trucdao" className="main-nav-link">Nhà Trường</a>
+						<a target={"_blank"} href="https://www.facebook.com/truong.trucdao" className="main-nav-link">Nhà Trường</a>
 					</li>
 					<li className="main-nav-item">
 						<a href="#hoatdong" className="main-nav-link">Hoạt Động</a>
@@ -22,7 +22,7 @@ export const HeaderSection = () => {
 						<a href="#chatbot" className="main-nav-link">Chat bot</a>
 					</li>
 					<li className="main-nav-item">
-						<a href="https://www.facebook.com/profile.php?id=100090833588628" className="main-nav-link nav-cta"
+						<a target={"_blank"} href="https://www.facebook.com/profile.php?id=100090833588628" className="main-nav-link nav-cta"
 						>Lớp Học</a
 						>
 					</li>
